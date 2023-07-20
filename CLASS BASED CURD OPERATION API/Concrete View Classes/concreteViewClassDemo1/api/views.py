dbb198fc0eb49d16e3f2d83b7a75a79f0aaf7a6c
alamin <alaminbhuyan321@gmail.com>
2023-07-20 07:59:41 +0600

    add response message

--- CLASS BASED CURD OPERATION API/Concrete View Classes/concreteViewClassDemo1/api/views.py
+++ CLASS BASED CURD OPERATION API/Concrete View Classes/concreteViewClassDemo1/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from api.serializers import EmployeeSerializer
 from api.models import Employee
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
+from rest_framework.generics import (ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView)
+from rest_framework.response import Response
 
 
 # Create your views here.
@@ -13,21 +14,43 @@ class EmployeeList(ListAPIView):
 class EmployeeCreate(CreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer=serializer)
+        return Response({'message': 'Employee created successfully.', 'employee': serializer.data})
 
 
 class EmployeeUpdate(UpdateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = 'id'
+    
+    def update(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({'message': 'Employee updated successfully.', 'employee': serializer.data})
 
 
 class EmployeeRetrieve(RetrieveAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = 'id'
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({'message': 'Employee retrieved successfully.', 'data': serializer.data})
 
 
 class EmployeeDelete(DestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = 'id'
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({'message': 'Employee deleted successfully.'})
